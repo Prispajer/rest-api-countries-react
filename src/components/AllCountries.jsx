cce6d815e462583467b849c54d42345dc8623781
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-12-30 23:59:20 +0100

    Mapped all countries and stylized them  initially

---
+++ src/components/AllCountries.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function AllCountries() {
+  const [countries, setCountries] = React.useState([]);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+
+        if (!response.ok) {
+          throw new Error("Wystąpił błąd podczas pobierania danych z API");
+        } else {
+          const data = await response.json();
+          setCountries(data);
+          console.log(data);
+        }
+      } catch (error) {
+        console.log("Błąd pobierania danych:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const mappedElements = countries.map((country) => (
+    <section key={country.name.official} className="country__container">
+      <div className="country__flag">
+        <img src={country.flags.svg} alt={`Flaga: ${country.name.common}`} />
+      </div>
+      <div className="country__informations">
+        <h3 className="country__name">{country.name.common}</h3>
+        <p className="country__information">
+          <span>{`Population: `}</span>
+          {country.population}
+        </p>
+        <p className="country__information">
+          <span>{`Region: `}</span>
+          {country.region}
+        </p>
+        <p className="country__information">
+          <span>{`Capital: `}</span>
+          {country.capital}
+        </p>
+      </div>
+    </section>
+  ));
+
+  return <section className="countries">{mappedElements}</section>;
+}
