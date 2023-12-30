5149d5766603920efaec83138abcbf9bb1ae4ba7
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-12-30 17:34:30 +0100

    Project start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,10 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="navbar__header">Where in the world?</h1>
+      <div className="navbar__theme">
+        <i className="fa-regular fa-moon"></i>
+        <span className="navbar__switch-theme">Dark Mode</span>
+      </div>
     </nav>
   );
 }
