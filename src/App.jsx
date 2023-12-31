f7f3fcd46b1b2edb6d8974f3609231632ba04c86
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-12-31 01:21:44 +0100

    Added react router to change routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import Navbar from "./components/Navbar";
 import Options from "./components/Options";
 import AllCountries from "./components/AllCountries";
-
+import SelectedCountry from "./components/SelectedCountry";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/main.scss";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Options />
-      <AllCountries />
+      <Router>
+        <Navbar />
+        <Options />
+        <Routes>
+          <Route path="/" element={<AllCountries />} />
+          <Route path="/country/:countryName" element={<SelectedCountry />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/SelectedCountry.jsx
@@ -0,0 +1,7 @@
+export default function SelectedCountry() {
+  return (
+    <div>
+      <p>siema</p>
+    </div>
+  );
+}
